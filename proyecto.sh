08cfeabc6df100a62e36095ff343e83d359d9e67
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-28 13:09:00 +0100

    Planteado clonar repositorio si es borrado

--- proyecto.sh
+++ proyecto.sh
@@ -27,6 +27,18 @@
 ############################
 ##       CONSTANTES       ##
 ############################
+AM="\033[1;33m"  ## Color Amarillo
+AZ="\033[1;34m"  ## Color Azul
+BL="\033[1;37m"  ## Color Blanco
+CY="\033[1;36m"  ## Color Cyan
+GR="\033[0;37m"  ## Color Gris
+MA="\033[1;35m"  ## Color Magenta
+RO="\033[1;31m"  ## Color Rojo
+VE="\033[1;32m"  ## Color Verde
+CL="\e[0m"       ## Limpiar colores
+
+WORKSCRIPT=''  ## Directorio principal del script
+USER=$(whoami)   ## Usuario que ejecuta el script
 
 ###########################
 ##       VARIABLES       ##
@@ -47,8 +59,18 @@ if [[ "$USER" = 'root']]; then
 fi
 
 ## Generar marca de tiempo y si hace más de 1 día que no se actualiza ejecutar un → git pull
-## Si no existe proyecto → git clone ¿? (Preguntar si descargar de nuevo)
 
+## Si no existe proyecto → git clone ¿? (Preguntar si descargar de nuevo)
+if [[ -d "$WORKSCRIPT" ]] || [[ -f "$WORKSCRIPT/main.sh" ]]; then
+    echo -e "$RO El programa principal ha sido movido de directorio$VE"
+    read -p '¿Clonar de nuevo el directorio en el mismo lugar? s/N' entrada
+    if [[ $entrada = 's']] || [[ "$entrada" = "S" ]]; then
+        echo -e "$VE Preparando para clonar repositorio$CL"
+        git clone https://github.com/fryntiz/Generador_Proyectos.git $WORKSCRIPT
+    else
+        echo -e "$VE No se clona el repositorio$CL"
+    fi
+fi
 
 
 
