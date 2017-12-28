b8bccf3dda0602057f8bb14053db6cdb3efac140
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-28 12:32:18 +0100

    Añadida cabecera con autoría, redes sociales y estilos

--- proyecto.sh
+++ proyecto.sh
@@ -1,1 +1,44 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+############################
+##     IMPORTACIONES      ##
+############################
+
+############################
+##       CONSTANTES       ##
+############################
+
+###########################
+##       VARIABLES       ##
+###########################
+
+###########################
+##       FUNCIONES       ##
+###########################
+
+###########################
+##       EJECUCIÓN       ##
+###########################
+
+
+exit 0
+
+
 ## Si no existe proyecto → git clone ¿? (Preguntar si descargar de nuevo)
