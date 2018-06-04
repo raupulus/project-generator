44f547d88fa194f2e4db4dae20bd22e1da8c5cb5
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-04 17:36:20 +0200

    Instalación básica

--- Angular/generar.sh
+++ Angular/generar.sh
@@ -32,3 +32,48 @@
 ############################
 ##        FUNCIONES       ##
 ############################
+angular_preconfiguraciones() {
+    echo -e "$VE Generando Preconfiguraciones para el nuevo proyecto$CL"
+}
+
+angular_instalacion() {
+    if [[ ! -x '/usr/bin/ng' ]]; then
+        echo -e "$RO No está instalado Angular desde npm$CL"
+        exit 1
+    fi
+
+    echo -e "$VE Creando proyecto con el nombre$RO $nombre$CL"
+
+    ng new "$nombre"
+}
+
+angular_postconfiguraciones() {
+    echo -e "$VE Generando Postconfiguraciones$CL"
+    local dirActual=$PWD
+    cd "$nombre" || exit 1
+    ownerApache
+    cd "$dirActual" || exit 1
+}
+
+###########################
+##       EJECUCIÓN       ##
+###########################
+generar_angular() {
+    echo -e "$VE Generador de proyecto YII Básico$CL"
+
+    ## Comprueba si ya existe un proyecto
+    compruebaExisteProyecto
+
+    angular_preconfiguraciones
+    angular_instalacion
+    angular_postconfiguraciones
+
+    ## Crear Base de Datos
+    #generarBD
+
+    ## Asigna permisos necesarios
+    #permisos
+
+    ## Preguntar si quiere inicializar repositorio y sincronizar con GitHub
+    inicializar_GIT
+}
