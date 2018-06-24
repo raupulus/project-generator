c56a0fad9ff9c551aa1959b95c746300f23fa2ab
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-24 03:21:09 +0200

    Creo archivo con los parámetros globales en la aplicación

---
+++ php-api/estructura/config/params.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'API_KEY' => '3d524a53c110e4c22463b10ed32cef9d', //referencia generado
+    // con MD5(uniqueid(<some_string>, true))
+];
