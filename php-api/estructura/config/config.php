becedf1d7c8a3c4c6485d80133f4bb7600783e80
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-24 03:13:28 +0200

    Agrego opciones para configurar slim

---
+++ php-api/estructura/config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+
+return [
+    'determineRouteBeforeAppMiddleware' => true,
+    'displayErrorDetails' => true,
+    'addContentLengthHeader' => false,
+];
